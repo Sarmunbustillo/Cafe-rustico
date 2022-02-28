6a06c299d5a72fd9b47263584de4019d59a3ea6b
Sarmun Bustillo <sarmun.b@gmail.com>
2022-02-28 18:26:22 +0100

    Add inter font

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
                     crossOrigin
                 />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&family=Poppins:wght@300;500;600;700;900&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700;900&display=swap"
                     rel="stylesheet"
                 />
             </Head>
